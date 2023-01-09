c0cd38d7e29890059522780390eb5398a13d2fdc
qqri <cqo1154@gmail.com>
2023-01-09 23:04:26 +0900

    perfomance test - redis sorting

--- LeaderBoard/src/test/java/com/example/leaderboard/SimpleTest.java
+++ LeaderBoard/src/test/java/com/example/leaderboard/SimpleTest.java
@@ -10,6 +10,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @SpringBootTest
 public class SimpleTest {
@@ -17,6 +18,33 @@ public class SimpleTest {
     @Autowired
     private RankingService rankingService;
 
+    @Test
+    void getRanks() {
+        
+        rankingService.getTopRank(1); // 의미없는 호출
+        
+        Instant before = Instant.now();
+        Long userRank = rankingService.getUserRanking("user_100");
+        Duration elapsed = Duration.between(before, Instant.now());
+
+        System.out.println(String.format("RankId(%d) - Took %d ms" , userRank, elapsed.getNano()  / 1000000 ));
+
+        before = Instant.now();
+        List<String> topRankers = rankingService.getTopRank(10);
+        elapsed = Duration.between(before, Instant.now());
+
+        System.out.println(String.format("Range - Took %d ms" , userRank, elapsed.getNano()  / 1000000 ));
+
+    }
+
+    @Test
+    void insertScore() {
+        for(int i = 0 ; i < 1000000 ; i++ ) {
+            int score = (int)(Math.random() * 1000000 ) ;
+            String userId = "user_"+ i;
+            rankingService.setUserScore(userId , score); // 한번수행하는데에 오래걸림
+        }
+    }
 
     @Test
     void inMemorySortPerformance() {
